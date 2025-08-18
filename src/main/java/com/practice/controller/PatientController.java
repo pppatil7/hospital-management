98c28da3409730356ea54b86fe81a9d09510456d
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-18 14:54:28 +0530

    api secured

---
+++ src/main/java/com/practice/controller/PatientController.java
@@ -0,0 +1,32 @@
+package com.practice.controller;
+
+import com.practice.dto.AppointmentResponseDto;
+import com.practice.dto.CreateAppointmentRequestDto;
+import com.practice.dto.PatientResponseDto;
+import com.practice.service.AppointmentService;
+import com.practice.service.PatientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/patients")
+@RequiredArgsConstructor
+public class PatientController {
+
+    private final PatientService patientService;
+    private final AppointmentService appointmentService;
+
+    @PostMapping("/appointments")
+    public ResponseEntity<AppointmentResponseDto> createNewAppointment(@RequestBody CreateAppointmentRequestDto createAppointmentRequestDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(appointmentService.createNewAppointment(createAppointmentRequestDto));
+    }
+
+    @GetMapping("/profile")
+    private ResponseEntity<PatientResponseDto> getPatientProfile() {
+        Long patientId = 4L;
+        return ResponseEntity.ok(patientService.getPatientById(patientId));
+    }
+
+}
---
+++ src/test/java/com/practice/InsuranceTests.java
@@ -0,0 +1,68 @@
+package com.practice;
+
+import com.practice.entity.Appointment;
+import com.practice.entity.Insurance;
+import com.practice.entity.Patient;
+import com.practice.service.AppointmentService;
+import com.practice.service.InsuranceService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@SpringBootTest
+public class InsuranceTests {
+
+    @Autowired
+    private InsuranceService insuranceService;
+
+    @Autowired
+    private AppointmentService appointmentService;
+
+    @Test
+    public void testInsurance() {
+        Insurance insurance = Insurance.builder()
+                .policyNumber("HDFC_1234")
+                .provider("HDFC")
+                .validUntil(LocalDate.of(2030, 12, 12))
+                .build();
+
+        Patient patient = insuranceService.assignInsuranceToPatient(insurance, 1L);
+
+        System.out.println(patient);
+
+        var newPatient = insuranceService.disaccociateInsuranceFromPatient(patient.getId());
+
+        System.out.println(newPatient);
+    }
+
+
+    @Test
+    public void testCreateAppointment() {
+        Appointment appointment = Appointment.builder()
+                .appointmentTime(LocalDateTime.of(2025, 11, 1, 14, 0, 0))
+                .reason("Cancer")
+                .build();
+
+//        var newAppointment = appointmentService.createNewAppointment(appointment, 1L, 2L);
+
+//        System.out.println(newAppointment);
+
+//        var updatedAppointment = appointmentService.reAssignAppointmentToAnotherDoctor(newAppointment.getId(), 3L);
+
+//        System.out.println(updatedAppointment);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
