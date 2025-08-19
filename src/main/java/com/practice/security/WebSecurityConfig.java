c4c48868dd4bf3b6641a4537c1867452d27f3769
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-19 16:49:18 +0530

    remove csrf,session,form

--- src/main/java/com/practice/security/WebSecurityConfig.java
+++ src/main/java/com/practice/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,12 +15,16 @@ public class WebSecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/public/**").permitAll()
+        httpSecurity
+                .csrf(csrfConfig -> csrfConfig.disable())
+                .sessionManagement(sessionConfig ->
+                        sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/public/**", "/auth/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/doctors/**").hasAnyRole("DOCTOR", "ADMIN")
-                )
-                .formLogin(Customizer.withDefaults());
+                );
+//                .formLogin(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
