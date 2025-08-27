e5a9e85df3e10eb3dc573be646549cc817109ee1
pp_ patil7 <pundalikpatil7218@gmail.com>
2025-08-27 13:16:08 +0530

    google oAuth

--- src/main/java/com/practice/security/AuthService.java
+++ src/main/java/com/practice/security/AuthService.java
@@ -4,12 +4,17 @@ import com.practice.controller.SignUpResponseDto;
 import com.practice.dto.LoginRequestDto;
 import com.practice.dto.LoginResponseDto;
 import com.practice.entity.User;
+import com.practice.entity.type.AuthProviderType;
 import com.practice.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -33,14 +38,48 @@ public class AuthService {
         return new LoginResponseDto(token, user.getId());
     }
 
-    public SignUpResponseDto signup(LoginRequestDto signupRequestDto) {
+    public User signUpInternal(LoginRequestDto signupRequestDto, AuthProviderType authProviderType, String providerId) {
         User user = userRepository.findByUsername(signupRequestDto.getUsername()).orElse(null);
         if (user != null) throw new IllegalArgumentException("User already exists");
-        user = userRepository.save(User.builder()
+        user = User.builder()
                 .username(signupRequestDto.getUsername())
-                .password(passwordEncoder.encode(signupRequestDto.getPassword()))
-                .build()
-        );
+                .providerId(providerId)
+                .providerType(authProviderType)
+                .build();
+        if (authProviderType == AuthProviderType.EMAIL) {
+            user.setPassword(passwordEncoder.encode(signupRequestDto.getPassword()));
+        }
+        return userRepository.save(user);
+    }
+
+    //login controller
+    public SignUpResponseDto signup(LoginRequestDto signupRequestDto) {
+        User user = signUpInternal(signupRequestDto, AuthProviderType.EMAIL, null);
         return new SignUpResponseDto(user.getId(), user.getUsername());
+    }
+
+    @Transactional
+    public ResponseEntity<LoginResponseDto> handleOAuth2LoginRequest(OAuth2User oAuth2User, String registrationId) {
+        AuthProviderType providerType = authUtil.getProviderTypeFromRegistrationId(registrationId);
+        String providerId = authUtil.determineProviderIdFromOAuth2User(oAuth2User, registrationId);
+        User user = userRepository.findByProviderIdAndProviderType(providerId, providerType).orElse(null);
+        String email = oAuth2User.getAttribute("email");
+
+        User emailUser = userRepository.findByUsername(email).orElse(null);
+
+        if (user == null && emailUser == null) {
+            //signup flow:
+            String username = authUtil.determineUsernameFromOAuth2User(oAuth2User, registrationId, providerId);
+            user = signUpInternal(new LoginRequestDto(username, null), providerType, providerId);
+        } else if (user != null) {
+            if (email != null && !email.isBlank() && !email.equals(user.getUsername())) {
+                user.setUsername(email);
+                userRepository.save(user);
+            }
+        } else {
+            throw new BadCredentialsException("This email is already registered with provider " + emailUser.getProviderType());
+        }
+        LoginResponseDto loginResponseDto = new LoginResponseDto(authUtil.generateAccessToken(user), user.getId());
+        return ResponseEntity.ok(loginResponseDto);
     }
 }
